b236a9b660c75f4637e720ed201db3ecbeb3c1df
JasmineQiuqiu <107149113+JasmineQiuqiuLi@users.noreply.github.com>
2024-07-21 16:23:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 from transformers import pipeline
 
-# load the pipeline
-sentiment_analysis = pipeline("sentiment-analysis")
-
 # title
 st.title('Analyze Sentiment of Text!')
 
@@ -15,9 +12,26 @@ st.write("Let's try it: :smiley:")
 
 user_input = st.text_input('Enter something below:')
 
+model_options = ["finiteautomata/bertweet-base-sentiment-analysis", 
+                 "nlptown/bert-base-multilingual-uncased-sentiment", 
+                 "bhadresh-savani/distilbert-base-uncased-emotion"]
+selected_model = st.selectbox('Choose a model:', model_options)
+
+def choose_model(option):
+    chosen_model = pipeline("sentiment-analysis", model=option)
+    return chosen_model
+
+# Initialize the model
+model = None
+if selected_model:
+    model = choose_model(selected_model)
+
 if st.button('Submit'):
     if user_input:
-        results = sentiment_analysis(user_input)
-        st.write('The detected sentiment is', result[0].label, "with a score of",result[0].score)
+        if model:
+            results = model(user_input)
+            st.write('The detected sentiment is', results[0]['label'], "with a score of", results[0]['score'])
+        else:
+            st.write('Please select a model!')
     else:
         st.write('Please enter something!')
